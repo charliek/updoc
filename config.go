dbbb30c66dc6184ae0b86618416c4ad8a8283129
Charlie Knudsen <cknudsen@bloomhealthco.com>
2014-06-04 19:53:10 -0500

    Add config tests

--- config.go
+++ config.go
@@ -1,27 +1,11 @@
 package main
 
-// {
-//   "name": "dtesting",
-//   "image": "charliek/docker-testing:green",
-//   "stdin": false,
-//   "stdout": true,
-//   "stderr": true,
-//   "ports": [
-//     {
-//       "protocol": "tcp",
-//       "hostPort": 9090,
-//       "containerPort": 9090,
-//     }
-//   ],
-//   "env": {
-//     "FOO": "BAR",
-//     "VAR2": "value"
-//   },
-//   "command": [
-//     "/bin/echo",
-//     "'12345' '6789'"
-//   ]
-// }
+import (
+	"encoding/json"
+	"fmt"
+	"io/ioutil"
+	"strings"
+)
 
 type PortMapping struct {
 	Protocol      string
@@ -31,11 +15,54 @@ type PortMapping struct {
 
 type UpdocApp struct {
 	Name    string
-	image   string
+	Image   string
 	Stdin   bool
 	Stdout  bool
 	Stderr  bool
 	Ports   []PortMapping
 	Env     map[string]string
 	Command []string
+}
+
+type appSlice []*UpdocApp
+
+func (apps appSlice) findByName(name string) *UpdocApp {
+	for _, app := range apps {
+		if app.Name == name {
+			return app
+		}
+	}
+	return nil
+}
+
+func LoadFromDir(dirPath string) (appSlice, error) {
+	files, err := ioutil.ReadDir(dirPath)
+	if err != nil {
+		return nil, err
+	}
+
+	apps := make(appSlice, 0, 3)
+	for _, file := range files {
+		if strings.HasSuffix(file.Name(), ".json") {
+			app, err := AppFromFile(fmt.Sprintf("%s/%s", dirPath, file.Name()))
+			if err != nil {
+				return nil, fmt.Errorf("Error loading app file %v: %v", file.Name(), err)
+			}
+			apps = append(apps, app)
+		}
+	}
+	return apps, nil
+}
+
+func AppFromFile(appFile string) (*UpdocApp, error) {
+	var app UpdocApp
+	appBytes, err := ioutil.ReadFile(appFile)
+	if err != nil {
+		return nil, fmt.Errorf("Error reading file %s: %v", appFile, err)
+	}
+
+	if err := json.Unmarshal(appBytes, &app); err != nil {
+		return nil, fmt.Errorf("Error unmarshaling json: %v", err)
+	}
+	return &app, nil
 }
---
+++ config_test.go
@@ -0,0 +1,76 @@
+package main
+
+import (
+	"reflect"
+	"testing"
+)
+
+var test1Expected = &UpdocApp{
+	Name:   "dtesting",
+	Image:  "charliek/docker-testing:green",
+	Stdin:  false,
+	Stdout: true,
+	Stderr: true,
+	Ports: []PortMapping{
+		PortMapping{
+			Protocol:      "tcp",
+			HostPort:      9090,
+			ContainerPort: 9090,
+		},
+	},
+	Env: map[string]string{
+		"FOO":  "BAR",
+		"VAR2": "value",
+	},
+	Command: []string{"/bin/echo", "'12345' '6789'"},
+}
+
+var test2Expected = &UpdocApp{
+	Name:   "pullreq",
+	Image:  "charliek/pullreq",
+	Stdin:  false,
+	Stdout: true,
+	Stderr: false,
+	Ports: []PortMapping{
+		PortMapping{
+			Protocol:      "udp",
+			HostPort:      22,
+			ContainerPort: 22,
+		},
+		PortMapping{
+			Protocol:      "tcp",
+			HostPort:      80,
+			ContainerPort: 443,
+		},
+	},
+	Env:     nil,
+	Command: nil,
+}
+
+func TestAppConfigLoading(t *testing.T) {
+	app, err := AppFromFile("examples/test1.json")
+	if err != nil {
+		t.Errorf("Error loading app config: %v", err)
+	}
+
+	if !reflect.DeepEqual(app, test1Expected) {
+		t.Errorf("Configs are not equal")
+	}
+}
+
+func TestLoadFromDir(t *testing.T) {
+	apps, err := LoadFromDir("examples")
+	if err != nil {
+		t.Errorf("Error loading app config: %v", err)
+	}
+
+	if len(apps) != 2 {
+		t.Errorf("Invalid number of apps %d", len(apps))
+	}
+	if !reflect.DeepEqual(apps[0], test1Expected) {
+		t.Errorf("test1 is not valid")
+	}
+	if !reflect.DeepEqual(apps[1], test2Expected) {
+		t.Errorf("test2 is not valid")
+	}
+}
